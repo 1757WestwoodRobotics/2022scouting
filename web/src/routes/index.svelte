4e88f308db6a6121fac0a239078da6e18787e0f8
Luke Maxwell <24lbmaxwell@protonmail.com>
2022-04-24 20:34:08 -0400

    no x axis overflow

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -92,129 +92,131 @@
 {#await promise}
   <p>fecthing...</p>
 {:then data}
-  <table>
-    <thead>
-      <tr>
-        <th>Team Name</th>
-        <th
-          on:click={() => {
-            sortingFunction = sortingMethods[0].fn;
-          }}>Team Number</th
-        >
-        <th
-          on:click={() => {
-            sortingFunction = sortingMethods[1].fn;
-          }}>Avg Teleop Cargo</th
-        >
-        <th
-          on:click={() => {
-            sortingFunction = sortingMethods[2].fn;
-          }}>Avg Auto Cargo</th
-        >
-        <th
-          on:click={() => {
-            sortingFunction = sortingMethods[3].fn;
-          }}>Teleop % shots made</th
-        >
-        <th
-          on:click={() => {
-            sortingFunction = sortingMethods[4].fn;
-          }}>Auto % shots made</th
-        >
-        <th
-          on:click={() => {
-            sortingFunction = sortingMethods[5].fn;
-          }}>Highest Climb Level (pts)</th
-        >
-        <th
-          on:click={() => {
-            sortingFunction = sortingMethods[6].fn;
-          }}>Avg Climb</th
-        >
-        <th
-          on:click={() => {
-            sortingFunction = sortingMethods[7].fn;
-          }}>Avg upper cargo</th
-        >
-        <th
-          on:click={() => {
-            sortingFunction = sortingMethods[8].fn;
-          }}>Avg lower cargo</th
-        >
-        <th
-          on:click={() => {
-            sortingFunction = sortingMethods[9].fn;
-          }}>Avg Cargo Points</th
-        >
-      </tr>
-    </thead>
-    <tbody>
-      {#each data.sort(sortingFunction) as team}
+  <div class="tableContainer">
+    <table>
+      <thead>
         <tr>
-          <a href="team/{team.team_number}">
-            <th>{team.nickname}</th>
-          </a>
-          <a href="team/{team.team_number}">
-            <th>{team.team_number}</th>
-          </a>
+          <th>Team Name</th>
           <th
-            style={genColor(
-              team.avgTeleopCargo,
-              Math.max(...data.map((a) => a.avgTeleopCargo))
-            )}>{limitSigfigs(team.avgTeleopCargo)}</th
+            on:click={() => {
+              sortingFunction = sortingMethods[0].fn;
+            }}>Team Number</th
           >
           <th
-            style={genColor(
-              team.avgAutoCargo,
-              Math.max(...data.map((a) => a.avgAutoCargo))
-            )}>{limitSigfigs(team.avgAutoCargo)}</th
+            on:click={() => {
+              sortingFunction = sortingMethods[1].fn;
+            }}>Avg Teleop Cargo</th
           >
           <th
-            style={genColor(
-              team.teleopConsistency,
-              Math.max(...data.map((a) => a.teleopConsistency))
-            )}>{limitSigfigs(team.teleopConsistency)}</th
+            on:click={() => {
+              sortingFunction = sortingMethods[2].fn;
+            }}>Avg Auto Cargo</th
           >
           <th
-            style={genColor(
-              team.autoConsistency,
-              Math.max(...data.map((a) => a.autoConsistency))
-            )}>{limitSigfigs(team.autoConsistency)}</th
+            on:click={() => {
+              sortingFunction = sortingMethods[3].fn;
+            }}>Teleop % shots made</th
           >
           <th
-            style={genColor(
-              team.highestClimb,
-              Math.max(...data.map((a) => a.highestClimb))
-            )}>{limitSigfigs(team.highestClimb)}</th
+            on:click={() => {
+              sortingFunction = sortingMethods[4].fn;
+            }}>Auto % shots made</th
           >
           <th
-            style={genColor(
-              team.avgClimb,
-              Math.max(...data.map((a) => a.avgClimb))
-            )}>{limitSigfigs(team.avgClimb)}</th
+            on:click={() => {
+              sortingFunction = sortingMethods[5].fn;
+            }}>Highest Climb Level (pts)</th
           >
           <th
-            style={genColor(
-              team.avgUpperCargo,
-              Math.max(...data.map((a) => a.avgUpperCargo))
-            )}>{limitSigfigs(team.avgUpperCargo)}</th
+            on:click={() => {
+              sortingFunction = sortingMethods[6].fn;
+            }}>Avg Climb</th
           >
           <th
-            style={genColor(
-              team.avgLowerCargo,
-              Math.max(...data.map((a) => a.avgLowerCargo))
-            )}>{limitSigfigs(team.avgLowerCargo)}</th
+            on:click={() => {
+              sortingFunction = sortingMethods[7].fn;
+            }}>Avg upper cargo</th
           >
           <th
-            style={genColor(
-              team.avgCargoPoints,
-              Math.max(...data.map((a) => a.avgCargoPoints))
-            )}>{limitSigfigs(team.avgCargoPoints)}</th
+            on:click={() => {
+              sortingFunction = sortingMethods[8].fn;
+            }}>Avg lower cargo</th
+          >
+          <th
+            on:click={() => {
+              sortingFunction = sortingMethods[9].fn;
+            }}>Avg Cargo Points</th
           >
         </tr>
-      {/each}
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        {#each data.sort(sortingFunction) as team}
+          <tr>
+            <a href="team/{team.team_number}">
+              <th>{team.nickname}</th>
+            </a>
+            <a href="team/{team.team_number}">
+              <th>{team.team_number}</th>
+            </a>
+            <th
+              style={genColor(
+                team.avgTeleopCargo,
+                Math.max(...data.map((a) => a.avgTeleopCargo))
+              )}>{limitSigfigs(team.avgTeleopCargo)}</th
+            >
+            <th
+              style={genColor(
+                team.avgAutoCargo,
+                Math.max(...data.map((a) => a.avgAutoCargo))
+              )}>{limitSigfigs(team.avgAutoCargo)}</th
+            >
+            <th
+              style={genColor(
+                team.teleopConsistency,
+                Math.max(...data.map((a) => a.teleopConsistency))
+              )}>{limitSigfigs(team.teleopConsistency)}</th
+            >
+            <th
+              style={genColor(
+                team.autoConsistency,
+                Math.max(...data.map((a) => a.autoConsistency))
+              )}>{limitSigfigs(team.autoConsistency)}</th
+            >
+            <th
+              style={genColor(
+                team.highestClimb,
+                Math.max(...data.map((a) => a.highestClimb))
+              )}>{limitSigfigs(team.highestClimb)}</th
+            >
+            <th
+              style={genColor(
+                team.avgClimb,
+                Math.max(...data.map((a) => a.avgClimb))
+              )}>{limitSigfigs(team.avgClimb)}</th
+            >
+            <th
+              style={genColor(
+                team.avgUpperCargo,
+                Math.max(...data.map((a) => a.avgUpperCargo))
+              )}>{limitSigfigs(team.avgUpperCargo)}</th
+            >
+            <th
+              style={genColor(
+                team.avgLowerCargo,
+                Math.max(...data.map((a) => a.avgLowerCargo))
+              )}>{limitSigfigs(team.avgLowerCargo)}</th
+            >
+            <th
+              style={genColor(
+                team.avgCargoPoints,
+                Math.max(...data.map((a) => a.avgCargoPoints))
+              )}>{limitSigfigs(team.avgCargoPoints)}</th
+            >
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
 {/await}
 
 <style>
@@ -241,6 +243,9 @@
   }
   thead tr th:hover {
     cursor: pointer;
+  }
+  .tableContainer {
+    overflow-x: scroll;
   }
 
   figure {
